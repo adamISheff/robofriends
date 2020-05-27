91e7ea48d19b5e23af662da3444a4321b06fb696
AdamiSheff <AdamiSheff@gmail.com>
2020-05-27 09:14:45 -0400

    Updates

---
+++ precache-manifest.4ebb33b119308ca18a83c4d32d0346df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29633f1367ce409cb95d5c128e1fe7b8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "83fabb1fc72863a7e567",
+    "url": "/robofriends/static/css/main.56183fd4.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83fabb1fc72863a7e567",
+    "url": "/robofriends/static/js/main.fe72bea9.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
